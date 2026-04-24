64de95fd72eacd48ad5576feb1853336e9366cda
roshanrojuk <roshanrojuk@gmail.com>
2026-04-24 09:32:37 +0530

    Create style.cs

---
+++ style.cs
@@ -0,0 +1,39 @@
+body {
+  margin: 0;
+  height: 100vh;
+  background: white;
+  color: black;
+  font-family: Arial, sans-serif;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.container {
+  text-align: center;
+}
+
+h1 {
+  font-size: 18px;
+  opacity: 0.6;
+  letter-spacing: 2px;
+}
+
+#time {
+  font-size: 70px;
+  margin: 30px 0;
+}
+
+button {
+  padding: 12px 25px;
+  font-size: 16px;
+  border: 1px solid black;
+  background: white;
+  cursor: pointer;
+}
+
+button:hover {
+  background: black;
+  color: white;
+}
